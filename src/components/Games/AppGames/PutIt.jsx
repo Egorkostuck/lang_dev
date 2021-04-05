fe9f13cf2d1a45a560e2881c9a0e0e8b69fd094a
MaryiaBond <dairymolko@gmail.com>
2021-04-05 15:35:27 +0300

    for PR

--- src/components/Games/AppGames/PutIt.jsx
+++ src/components/Games/AppGames/PutIt.jsx
@@ -1,47 +1,54 @@
-import React, {useContext, useMemo, useState} from 'react';
+import React, {useContext, useEffect, useMemo, useState} from 'react';
 import Store from './../../../context';
 import styles from './AppGames.module.css';
 
 const PutIt = ({nextWord}) => {
     const data = useContext(Store);
     const randomWords = useMemo(() => data.playWords.sort(() => Math.random() - 0.5), []);
-    const splitWords = randomWords[data.wordIndex].translate.split('').sort();
+   
     const [arrPutLetter, setArrPutLetter] = useState([]);
-    const [deleteLatter, setDeleteLetter] = useState(splitWords);
+    const [splitWords, setSplitWords] = useState([]);
+    useEffect(() => {
+        setSplitWords(randomWords[data.wordIndex].translate.split('').sort())
+    }, [data.wordIndex])
     
     const putWord = (item) => {
-        setArrPutLetter([...arrPutLetter, item]);
-        let b = deleteLatter.join('').replace(item, '').split('');
-        setDeleteLetter(b);
-        // debugger
+        const currentLettersArray = [...arrPutLetter, item]
+        const currentWord = randomWords[data.wordIndex].translate
+        setArrPutLetter(currentLettersArray);
+        setSplitWords(splitWords.join('').replace(item, '').split(''))
+        if(arrPutLetter.length === currentWord.length - 1) {
+            let fullWord = currentLettersArray.join('');
+            if (fullWord === currentWord) {
+                data.setCorrectWords(data.correctWords + 1);            
+                nextWord();
+                setArrPutLetter([]);           
+            } else {
+                data.setErrorWords(data.errorWords + 1);            
+                nextWord();
+                setArrPutLetter([]);          
+            }
+        }  
     }
 
-    if(arrPutLetter.length === splitWords.length) {
-        let fullWord = arrPutLetter.join('');
-        if (fullWord === randomWords[data.wordIndex].translate) {
-            setDeleteLetter(splitWords);
-            data.setCorrectWords(data.correctWords + 1);            
-            nextWord();
-            setArrPutLetter([]);           
-        } else {
-            setDeleteLetter(splitWords);
-            data.setErrorWords(data.errorWords + 1);            
-            nextWord();
-            setArrPutLetter([]);          
-        }
-        // debugger
-    }  
+    const returnWord = (letter) => {
+        const letterIndex = arrPutLetter.indexOf(letter)
+        setArrPutLetter(arrPutLetter.filter((item, index) => letterIndex !== index))
+        setSplitWords([...splitWords, letter])
+    }
+
+
     
     return(
         <section>
             <span>Put together a translation</span>
             <div className={styles.putWordsContainer}>
                 {arrPutLetter.map((letter,index) => (
-                    <div key={index} className={styles.putWordsLetter}>{letter}</div>
+                    <div key={index} onClick={() => returnWord(letter)} className={styles.putWordsLetter}>{letter}</div>
                 ))}
             </div>
             <div className={styles.splitWordsContainer}>
-                {deleteLatter.map((item, index) => (
+                {splitWords.map((item, index) => (
                     <div className={styles.splitWordsLetter} key={index} onClick={() => putWord(item)}>{item}</div>
                 ))}
             </div>
